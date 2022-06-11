df3778584aeb4c9ec9baef0c3d07e72ea4484619
adam.vert <vertadam@gmail.com>
2022-06-11 16:17:41 -0400

    Updated allowed hosts

--- paint_the_world/settings.py
+++ paint_the_world/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['paint-the-world.herokuapp.com']
 
 
 
