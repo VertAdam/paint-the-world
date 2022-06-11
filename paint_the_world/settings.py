62ad776d3af390b648eb0362bfa7695588aab210
adam.vert <vertadam@gmail.com>
2022-06-11 19:14:15 -0400

    Debug false

--- paint_the_world/settings.py
+++ paint_the_world/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
 ALLOWED_HOSTS = ['paint-the-world.herokuapp.com']
 
